d3e39b8046141168ba48f111fb2654414297766c
salunkhegr1712 <salunkhegr1712@gmail.com>
2025-06-09 21:46:31 +0530

    [JS] started with javascript to parallely

---
+++ javascript/first.js
@@ -0,0 +1,11 @@
+// console.error is used to print the error 
+// are used for single line comment 
+
+/* while
+    these tags are use to have multiline comments
+    like this example
+*/ 
+
+// javascript can be used in front end or server side 
+// javascript can be used in client (user side) or server side 
+console.error("error")
